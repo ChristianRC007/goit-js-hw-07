e8fb03c8be4f39774d5a19e33d033ce2e04655e7
Andrey Cherniak <baza904@gmail.com>
2021-02-08 13:11:45 +0200

    updates task-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,8 +1,9 @@
-const ingredients = [
-  'Картошка',
-  'Грибы',
-  'Чеснок',
-  'Помидоры',
-  'Зелень',
-  'Приправы',
-];
+const ingredients = ["Картошка", "Грибы", "Чеснок", "Помидоры", "Зелень", "Приправы"];
+
+const ingredientsListEl = document.querySelector("#ingredients");
+
+ingredients.forEach(x => {
+  const listItemEl = document.createElement("li");
+  listItemEl.textContent = x;
+  ingredientsListEl.appendChild(listItemEl);
+});
