21abc5f3ef40cfe5d4ad7a17bbf60b74bb006edf
Andrey Cherniak <baza904@gmail.com>
2021-03-03 12:39:28 +0200

    make it prettier

--- js/task-8.js
+++ js/task-8.js
@@ -2,7 +2,7 @@ const refs = {
   input: document.querySelector('#controls > input'),
   renderBtn: document.querySelector('button[data-action="render"]'),
   destroyBtn: document.querySelector('button[data-action="destroy"]'),
-  boxesListEl: document.querySelector('#boxes')
+  boxesListEl: document.querySelector('#boxes'),
 };
 
 refs.input.addEventListener('input', getInputValue);
@@ -10,29 +10,29 @@ refs.renderBtn.addEventListener('click', renderBoxes);
 refs.destroyBtn.addEventListener('click', destroyBoxes);
 
 function getInputValue() {
-  return refs.input.value;  
-};
+  return refs.input.value;
+}
 
 function renderBoxes() {
-  createBoxes(getInputValue())
-};
+  createBoxes(getInputValue());
+}
 
 function createBoxes(amount) {
   const elementsArray = [];
 
   for (let i = 0; i < amount; i += 1) {
-    const randomColor = Math.floor(Math.random() * 16777215).toString(16)
-    let boxEl = document.createElement("div");
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    let boxEl = document.createElement('div');
     let boxSize = 30;
-    
-    boxEl.style.backgroundColor = `#${randomColor}`
-    boxEl.style.height = boxEl.style.width = `${boxSize + i * 10}px`
 
-    elementsArray.push(boxEl)
+    boxEl.style.backgroundColor = `#${randomColor}`;
+    boxEl.style.height = boxEl.style.width = `${boxSize + i * 10}px`;
+
+    elementsArray.push(boxEl);
   }
-  refs.boxesListEl.append(...elementsArray)
-};
+  refs.boxesListEl.append(...elementsArray);
+}
 
 function destroyBoxes() {
-  refs.boxesListEl.innerHTML = ''
-};
+  refs.boxesListEl.innerHTML = '';
+}
